3e26e9abc58fd742a570e1620f4f2bf970020e82
Faithy.dev <fathiaolusegun@gmail.com>
2025-08-10 11:01:38 +0100

    Update index.tsx

--- alx-project-0x09/pages/index.tsx
+++ alx-project-0x09/pages/index.tsx
@@ -15,6 +15,7 @@ const Home: React.FC = () => {
 
   const handleGenerateImage = async () => {
     console.log("Generating Images");
+    console.log(process.env.NEXT_PUBLIC_GPT_API_KEY);
   };
 
   return (
@@ -50,4 +51,7 @@ const Home: React.FC = () => {
   );
 };
 
-export default Home;
+export default Home; 
+
+
+
