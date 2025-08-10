4fbd218d347d1ca40d96300ef3fbc2b188474a10
Faithy.dev <fathiaolusegun@gmail.com>
2025-08-10 11:17:03 +0100

    Update index.ts

--- alx-project-0x09/interfaces/index.ts
+++ alx-project-0x09/interfaces/index.ts
@@ -1,9 +1,8 @@
 import { ReactNode } from "react";
 
 export interface ReactComponentProps {
-  children: ReactNode
+    children: ReactNode
 }
-
 
 export interface GeneratedImageProps {
   imageUrl: string
@@ -11,6 +10,10 @@ export interface GeneratedImageProps {
   width?: string
   height?: string
   action: (imagePath: string) => void
+}
+
+export type RequestProps = {
+	prompt: string;
 }
 
 export type ImageProps = Pick<GeneratedImageProps, "imageUrl" | "prompt">
